3b33eebbac6c52ada2f895324f731187e9645ce5
BBEARS <134229346+BlockBearsV1@users.noreply.github.com>
2023-07-17 17:22:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ Initialize the SDK and contract on your project:
 import { ThirdwebSDK } from "@thirdweb-dev/sdk/evm";
 
 const sdk = new ThirdwebSDK("polygon", {
-  clientId: "YOUR_CLIENT_ID", // Use client id if using on the client side, get it from dashboard settings
+  clientId: "120d600347fd4f4572fe311e2dc07df5", // Use client id if using on the client side, get it from dashboard settings
   secretKey: "YOUR_SECRET_KEY", // Use secret key if using on the server, get it from dashboard settings
 });
 const contract = await sdk.getContract("0xcE221D36AC137EDc29335c57c320Ad0Dade6279B");
