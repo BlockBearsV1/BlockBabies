b3f0a7174e2e8fcd3ff137ffd0878fab25424cc2
BBEARS <134229346+BlockBearsV1@users.noreply.github.com>
2023-07-20 22:56:41 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk/evm";
 
 const sdk = new ThirdwebSDK("polygon", {
   clientId: "120d600347fd4f4572fe311e2dc07df5", // Use client id if using on the client side, get it from dashboard settings
-  secretKey: "yL-ussjO2woOBAvljEeaSnWYH-PwZ3ApkIycfEE4w4SBmwfT1-mbBzOGQWyLtnFiHYmqV-YJhSRh5cJ7X0kVTw", // Use secret key if using on the server, get it from dashboard settings
+  secretKey: "", // Use secret key if using on the server, get it from dashboard settings
 });
 const contract = await sdk.getContract("0xcE221D36AC137EDc29335c57c320Ad0Dade6279B");
 
